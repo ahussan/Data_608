fa117609121de6b5cdfde71a15dbe595be12ee38
Ahussan <ahussan@users.noreply.github.com>
2020-12-12 22:27:27 -0500

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-import dash
-import resources
-
-app = dash.Dash(__name__, external_stylesheets=resources.external_stylesheets)
-server = app.server
-app.config['suppress_callback_exceptions'] = True
